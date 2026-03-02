a9f16ef646a84d7676af43efe64763eb925770ff
Jnxyan <janiceng040803@gmail.com>
2026-03-03 00:25:54 +0800

    try Plant class

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -1,9 +1,13 @@
+import java.awt.desktop.SystemEventListener;
+
 public class MainApp {
     public static void main(String[] args){
         Plant chili = new Plant(80, 60, 100);
 
         chili.getN();
+        System.out.println(chili.getN());
         chili.setN(10);
         chili.getN();
+        System.out.println(chili.getN());
     }
 }
--- src/main/java/Plant.java
+++ src/main/java/Plant.java
@@ -15,7 +15,6 @@ public class Plant {
     //3. getter and setter
     //getter
     public  int getN() {
-        System.out.print(n);
         return n;
     }
     public void setN(int n) {
