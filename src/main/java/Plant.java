43e927d991aaf336efae2b6e5651630495a09d02
Jnxyan <janiceng040803@gmail.com>
2026-03-03 00:21:00 +0800

    Plant class

--- src/main/java/Plant.java
+++ src/main/java/Plant.java
@@ -1,3 +1,12 @@
 public class Plant {
-//hello //hiiii
+    //1. data field
+    private int n;
+    private int p;
+    private int k;
+    //seven data
+
+    //2. constructor
+    public Plant(int n, int p, int k){
+        this.n = n; //this (n) = above de private int n
+    }
 }
