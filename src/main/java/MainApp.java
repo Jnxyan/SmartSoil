6414a2af7be96e41efe93055539f2a1c67728f17
Nicole <231361@student.upm.edu.my>
2026-03-06 17:01:02 +0800

    a

--- src/main/java/MainApp.java
+++ src/main/java/MainApp.java
@@ -8,10 +8,6 @@ public class MainApp {
         List<Plant> plants = new ArrayList<>();
         // add all plants with ranges from your Excel
         plants.add(new Plant("Chili", 80,120,40,60,150,250,5.8,6.8,1200,2200,25,32,60,80));
-        plants.add(new Plant("Durian", 60,100,30,50,200,400,5.5,6.5,800,1500,24,32,40,60));
-        plants.add(new Plant("Paddy", 100,150,20,40,40,80,5.0,7.0,500,1200,25,35,80,100));
-        plants.add(new Plant("Oil Palm",120,180,30,50,150,300,4.0,5.5,400,800,24,33,70,90));
-        plants.add(new Plant("Leafy",150,200,40,70,100,180,6.0,7.0,1500,2500,20,30,50,70));
 
         Scanner scanner = new Scanner(System.in);
         System.out.print("Enter Nitrogen (N): ");
